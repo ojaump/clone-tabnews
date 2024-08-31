2cc2ef60ed26e65c636d6c31fc1ed2782c5ccd4c
João Pedro <neera.games@gmail.com>
2024-08-31 20:14:41 +0000

    Adicionado modificalções na pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
 function Home() {
-    return <h1>Teste 1</h1>
+    function redir() {
+        window.location.href = "https://www.youtube.com/watch?v=17_ucZl7MVo"
+    }
+    function styles() {
+        backgroundColor: "#ccc"
+        flex: "1"
+        fontSize: "20px"
+        margin: "10px"
+        textDecoration: "none"
+    }
+    return (
+        <>
+            <div style={{ backgroundColor: "#ccc", flex: "1" }}>
+                <h1>Oi Ana, to usando meu pedacinho de internet pra te dizer</h1>
+                <div style={{ fontSize: "20px", fontWeight:"bold" }}>que eu gosto muito de você e de sua amizade.
+                </div>
+                <div style={{ fontSize: "20px", fontWeight:"bold" }}>Adoro sua timeline maluca que sempre me tira umas boas gaitadas, suas otimas indicações de jogos e os papos de nerd ☝️🤓</div>
+                <div style={{ fontSize: "20px", fontWeight:"bold" }}>Fica bem! Te adoro ❤️✨</div>
+                <button onClick={redir} style={{ margin: "10px", textDecoration: "none" }}> Clica aqui.</button>
+            </div>
+        </>
+    )
 }
 
 export default Home; 
